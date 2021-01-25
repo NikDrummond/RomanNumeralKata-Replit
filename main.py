e53053d305cc08ce61e4301639ba81ce6b864dd5
Nik Drummond <nikolasdrummond@gmail.com>
2021-01-25 16:27:03 +0000

    First day draft

--- main.py
+++ main.py
@@ -2,19 +2,58 @@ import pytest
 
 
 # Source Code:
+lookup = {'I':1, 'V':5}
+
 
 def from_roman(roman: str) -> int:
-  return 100
+
+  # invert list here
+  char = [roman[i] for i in list(range(len(roman)-1,-1,-1))]
+  
+  #char = [i for i in roman]
+
+  # think about "special" case where not "I" is preceded by "I"
+ 
+
+
+  number = 0
+  # need to change this - indexing is off (-1)
+  for i, rom in enumerate(char):
+    if rom =="V":
+      return 5
+    
+
+    # i is an integer, the keys in the dict. are strings
+
+    # invert dictionary?
+    number += lookup[i]
+    
+        
+  return number
 
 
 
 # Tests: 
 
+# lets add to this hahaha
+
 cases = [
   ('I', 1),
-  # ('II', 2),
-  # ('III', 3),
+  ('II', 2),
+  ('III', 3),
 ]
 @pytest.mark.parametrize(['num', 'roman'], cases)
 def test_roman_1(num: int, roman: str):
   assert from_roman(num) == roman
+
+
+
+#for i in range(len(char)):
+#  if char[i] == "V":
+#    if char[i+1] == "I":
+#      number += lookup[i]-1
+#      i = i+2
+#    else:
+#      number += lookup[i]
+#  else:
+#    number += lookup[i]
